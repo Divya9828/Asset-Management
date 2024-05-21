1464f21b5747b04c11dfb1a25446c432a08b0918
Divya <vebbox.divya@gmail.com>
2024-05-21 10:15:02 +0530

    assethistory -view

--- routes/assetHistory.js
+++ routes/assetHistory.js
@@ -1,12 +1,15 @@
 // routes/assetRoutes.js
 const express = require('express');
 const router = express.Router();
-const {getAllAssets,getAssetById,createAsset,updateAsset,deleteAsset} = require('../controllers/assetHistoryController');
+const {getAllAssets,getAssetById,renderAddHis,createAsset,updateAsset,deleteAsset} = require('../controllers/assetHistoryController');
 
 // Define routes for CRUD operations on assets
-router.get('/getAllAssets', getAllAssets);
-router.get('/getAssetById', getAssetById);
-router.post('/createAsset', createAsset);
+router.get('/', getAllAssets);
+router.get('/:id', getAssetById);
+
+router.get('/add',renderAddHis)    
+router.post('/', createAsset);
+
 router.put('/updateAsset', updateAsset);
 router.delete('/deleteAsset', deleteAsset);
 
